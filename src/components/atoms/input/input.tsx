a448420a9f55aecaa8770735a274d1e53d9e1ad1
Otloir <oli.toreland@gmail.com>
2026-05-06 13:21:27 +0200

    created a basic input filed component. Also removed premade text in app

---
+++ src/components/atoms/input/input.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+interface NumberInputProps {
+  label?: string;
+  type?: "number" | "text";
+  min?: number;
+  max?: number;
+}
+
+export default function Input({
+  label = "Quantity",
+  type = "number",
+  min = 0,
+  max = 99,
+}: NumberInputProps) {
+  const [value, setValue] = useState<string | number>(min);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let newValue: string | number = e.target.value;
+
+    if (type === "number") {
+      const num = parseInt(newValue, 10);
+      if (!isNaN(num)) {
+        newValue = Math.max(min, Math.min(num, max));
+      }
+    }
+
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <label htmlFor="input">{label}</label>
+      <input
+        id="input"
+        type={type}
+        value={value}
+        onChange={handleChange}
+        {...(type === "number" && { min, max })}
+      />
+    </div>
+  );
+}
