4bcb3db7fe57d2356b054b0f65874722688ab0c9
Olof Björn <olof.v.bjorn@gmail.com>
2026-05-08 10:59:03 +0200

    Reworked item and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "./lib/supabase"; // use the shared client
-import Item from "./components/atoms/Item"; // example component to display shop items
+import Item from "./components/atoms/Item/Item"; // example component to display shop items
 
 
 function App() {
---
+++ src/types/item.types.ts
@@ -0,0 +1,10 @@
+// types/item.types.ts
+export type ItemRow = {
+  id: number;
+  name: string;
+  property: string;
+  propvalue: number;
+  description: string;
+  price: number;
+  img: string;
+};
