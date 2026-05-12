6ca792154b7c94424ca6760af3bf6e215794714e
Otloir <oli.toreland@gmail.com>
2026-05-13 00:36:47 +0200

    added shopItemList back because i remembered that inventory on desktop is a list and not a grid like in shop

--- src/components/molecules/itemList/ItemList.tsx
+++ src/components/molecules/itemList/ItemList.tsx
@@ -33,7 +33,7 @@ export default function ItemList({ store, variant, userId }: ListProps) {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className={styles.itemList}>
+    <div className={store ? styles.shopItemList : styles.itemList}>
       {items.map((item) => (
         <Item key={item.id} itemId={item.id} variant={variant} store={store} />
       ))}
--- src/routes/Inventory.tsx
+++ src/routes/Inventory.tsx
@@ -5,7 +5,7 @@ interface InventoryItemProps {
 }
 export default function Inventory({ userId }: InventoryItemProps) {
 // hardcoded user id. update to a dynamic one when we get ids from the centralbank
-  userId = 2;
+  userId = 1;
   return (
     <>
       <ItemList userId={userId} variant="row" />
