d1384118046ad0954acb1ba9a33c7dfd582db86c
Otloir <oli.toreland@gmail.com>
2026-05-14 15:25:30 +0200

    added a buttn that navigates back to lobby

--- src/components/views/Store/StorePage.tsx
+++ src/components/views/Store/StorePage.tsx
@@ -1,9 +1,19 @@
+import Button from "../../atoms/buttons/Button";
 import ItemList from "../../molecules/itemList/ItemList";
+import { useNavigate } from "react-router-dom";
 
 export default function StorePage() {
+  const navigate = useNavigate();
+
+  const navigateLobby = () => {
+    navigate("/");
+  };
+
   return (
     <>
+      <h1>Item Shop</h1>
       <ItemList type="store" variant="card" />
+      <Button onClick={navigateLobby}> Back to select </Button>
     </>
   );
 }
