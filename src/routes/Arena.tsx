51e3ee993dc44d015c9cc6dc25968499a0016f98
Olof Björn <olof.v.bjorn@gmail.com>
2026-05-17 14:25:40 +0200

    Last fix

--- src/routes/Arena.tsx
+++ src/routes/Arena.tsx
@@ -1,17 +1,9 @@
-  import BattleArena from "../components/organisms/BattleArena/BattleArena";
+import ArenaPage from "../components/views/Arena/ArenaPage";
 
-  export default function Arena() {
-    // FIX: make more dynamic later with multiplayer and bot/singleplayer version
-    // player 1 = the user
-    // player 2 = opponent
-    return (
-      <>
-        <BattleArena
-          playerOneId={1}
-          playerTwoId={2}
-          playerOneCreatureId={1}
-          playerTwoCreatureId={3}
-        />
-      </>
-    );
-  }
+export default function Arena() {
+  return (
+    <>
+      <ArenaPage />
+    </>
+  );
+}
